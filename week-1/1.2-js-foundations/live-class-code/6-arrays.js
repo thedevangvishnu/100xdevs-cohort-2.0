885a55c270a499d19df2d85434fe49a3560e7fd5
Devang Vishnu <thedevangvishnu@gmail.com>
2023-12-21 09:42:12 +0530

    Add one more program in 6-arrays.js
    Write a program in the same file using the same array that
    reverses all the elements of the array.

--- week-1/1.2-js-foundations/live-class-code/6-arrays.js
+++ week-1/1.2-js-foundations/live-class-code/6-arrays.js
@@ -1,6 +1,8 @@
 // Write a program that prints all the even numbers in an array
 // Write a program to print the biggest number in an array
+// Write a program that reverses all the elements of an array
 
+// 1
 const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 for (let i = 0; i < array.length; i++) {
   if (array[i] % 2 === 0) {
@@ -9,6 +11,7 @@ for (let i = 0; i < array.length; i++) {
 }
 // Output: 0, 2, 4, 6, 8
 
+// 2
 let biggestNumber = array[0];
 for (let i = 0; i < array.length; i++) {
   if (array[i] > biggestNumber) {
@@ -16,3 +19,10 @@ for (let i = 0; i < array.length; i++) {
   }
 }
 console.log("The biggest number of this array is " + biggestNumber); // 9
+
+// 3
+let reversedArray = [];
+for (let i = 0; i < array.length; i++) {
+  reversedArray[i] = array[array.length - 1 - i];
+}
+console.log(reversedArray); // [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
