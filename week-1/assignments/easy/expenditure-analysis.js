d7699eba523c855f0ffcb912b5e6306991a7839a
Devang Vishnu <thedevangvishnu@gmail.com>
2023-12-24 20:35:59 +0530

    Solve the expenditure-analysis.js problem

--- week-1/assignments/easy/expenditure-analysis.js
+++ week-1/assignments/easy/expenditure-analysis.js
@@ -14,7 +14,37 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let output = [];
+
+  // list of all the categories
+  let allCategories = [];
+  for (const transaction of transactions) {
+    allCategories.push(transaction.category);
+  }
+
+  // remove the repeated category items
+  const categories = allCategories.filter(
+    (item, index) => allCategories.indexOf(item) === index
+  );
+
+  // add the price of those items that are listed in transactions list and whose category matches the category-item in categories list.
+  for (let i = 0; i < categories.length; i++) {
+    let outputItem = {}; // each outputItem has two properties: category and totalSpent
+    outputItem.category = `${categories[i]}`;
+
+    let priceOfAllItemsInCategory = 0;
+
+    for (let j = 0; j < transactions.length; j++) {
+      if (categories[i] === transactions[j].category) {
+        priceOfAllItemsInCategory += transactions[j].price;
+      }
+    }
+
+    outputItem.totalSpent = priceOfAllItemsInCategory;
+
+    output.push(outputItem);
+  }
+  return output;
 }
 
 module.exports = calculateTotalSpentByCategory;
