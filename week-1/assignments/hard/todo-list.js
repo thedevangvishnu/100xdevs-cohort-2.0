9eef35dbc7a581c4abe4bdd6909e6a663860a7f5
Devang Vishnu <thedevangvishnu@gmail.com>
2023-12-25 12:03:45 +0530

    Solve todo-list.js problem

--- week-1/assignments/hard/todo-list.js
+++ week-1/assignments/hard/todo-list.js
@@ -11,7 +11,46 @@
 */
 
 class Todo {
+  toDos; // this is a list of all to-dos
+  constructor() {
+    this.toDos = [];
+  }
 
+  add(toDo) {
+    this.toDos.push(toDo);
+  }
+
+  remove(indexOfTodo) {
+    this.toDos.splice(indexOfTodo, 1);
+    console.log(this.toDos);
+  }
+
+  update(index, updatedTodo) {
+    if (index >= 0 && index < this.toDos.length) {
+      this.toDos[index] = updatedTodo;
+      return this.toDos;
+    } else {
+      return null;
+    }
+  }
+
+  getAll() {
+    return this.toDos;
+  }
+
+  get(indexOfTodo) {
+    // index need to be in the range of the list
+    if (indexOfTodo >= 0 && indexOfTodo < this.toDos.length) {
+      return this.toDos[indexOfTodo];
+    } else {
+      //  index is out of range for this array. Hence, returning null (as expected by tests
+      return null;
+    }
+  }
+
+  clear() {
+    this.toDos.splice(0, this.toDos.length);
+  }
 }
 
 module.exports = Todo;
