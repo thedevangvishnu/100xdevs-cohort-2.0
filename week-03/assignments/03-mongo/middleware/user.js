e9d9db5455b9c2b22194e0c68a329d897d708783
Devang Vishnu <thedevangvishnu@gmail.com>
2024-01-26 17:20:22 +0530

    Solve 03 assignment of week 03

--- week-03/assignments/03-mongo/middleware/user.js
+++ week-03/assignments/03-mongo/middleware/user.js
@@ -1,6 +1,20 @@
-function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+const { User } = require("../db/index");
+
+async function userMiddleware(req, res, next) {
+  // Implement user auth logic
+  // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+  const username = req.headers.username;
+  const password = req.headers.password;
+  const user = await User.findOne({
+    username: username,
+    password: password,
+  });
+
+  if (user) {
+    next();
+  } else {
+    res.status(403).json({ msg: "User doesn't exist" });
+  }
 }
 
 module.exports = userMiddleware;
