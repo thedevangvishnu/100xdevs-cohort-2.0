4cabfa6610fec03ca24119d44105aab67dfdfd65
Devang Vishnu <thedevangvishnu@gmail.com>
2024-01-27 09:00:12 +0530

    Create endpoints and use mongodb

--- week-05/5.2-project-building/fullstack-todo-app/backend/index.js
+++ week-05/5.2-project-building/fullstack-todo-app/backend/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
+const { Todo } = require("./database/db");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/todos", (req, res) => {
-  res.send("Hello...");
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+  return res.status(200).json({ tooos: todos });
 });
 
-app.post("/todos", (req, res) => {
+app.post("/todos", async (req, res) => {
   const createPayload = req.body;
   const parsePayload = createTodo.safeParse(createPayload);
 
@@ -17,12 +19,20 @@ app.post("/todos", (req, res) => {
     return res
       .status(411)
       .json({ error: "Invalid input properties and/or values" });
-  } else {
-    // push to mongodb
   }
+
+  const { title, description } = createPayload;
+
+  await Todo.create({
+    title,
+    description,
+    completed: false,
+  });
+
+  return res.status(201).json({ message: "Todo created successfully" });
 });
 
-app.put("/completed", (req, res) => {
+app.put("/completed", async (req, res) => {
   const createPayload = req.body;
   const parsePayload = updateTodo.safeParse(createPayload);
 
@@ -30,9 +40,19 @@ app.put("/completed", (req, res) => {
     return res
       .status(411)
       .json({ error: "Invalid input properties and/or values" });
-  } else {
-    // update the todo with this specific id
   }
+
+  // update the todo with this specific id
+  await Todo.updateOne(
+    {
+      _id: createPayload.id,
+    },
+    {
+      completed: true,
+    }
+  );
+
+  return res.status(201).json({ message: "Todo updated successfully" });
 });
 
 app.listen(3000);
