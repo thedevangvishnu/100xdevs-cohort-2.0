1c1c0a7d7f93953f6458aa5a8879513355f49965
Devang Vishnu <thedevangvishnu@gmail.com>
2024-02-19 10:44:55 +0530

    Solve useEffect assigments given in class

--- week-06/6.2-useEffect-useMemo-useCallback/live-class-code/useEffect-hook/src/App.jsx
+++ week-06/6.2-useEffect-useMemo-useCallback/live-class-code/useEffect-hook/src/App.jsx
@@ -3,31 +3,65 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [id, setId] = useState(1);
 
-  useEffect(() => {
-    fetch("https://sum-server.100xdevs.com/todos").then(async (response) => {
-      const data = await response.json();
-      setTodos(data.todos);
-    });
-  }, []);
+  const todoIds = [1, 2, 3, 4, 5];
+
+  // const [todos, setTodos] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("https://sum-server.100xdevs.com/todos").then(async (response) => {
+  //     const data = await response.json();
+  //     setTodos(data.todos);
+  //   });
+  // }, []);
 
   return (
     <div>
-      {todos.map((todo) => (
+      {/* {todos.map((todo) => (
         <Todo key={todo.id} todo={todo} />
-      ))}
+      ))} */}
+      <div style={{ display: "flex", gap: "1rem" }}>
+        {todoIds.map((itemId) => (
+          <Button id={itemId} onClick={() => setId(itemId)} />
+        ))}
+      </div>
+      <TodoById id={id} />
     </div>
   );
 }
 
-function Todo({ todo }) {
+// function Todo({ todo }) {
+//   return (
+//     <div>
+//       <h2>{todo.title}</h2>
+//       <p>{todo.description}</p>
+//     </div>
+//   );
+// }
+
+function TodoById({ id }) {
+  const [todo, setTodo] = useState({});
+
+  useEffect(() => {
+    fetch(`https://sum-server.100xdevs.com/todo?id=${id}`).then(
+      async (response) => {
+        const todoData = await response.json();
+        setTodo(todoData.todo);
+      }
+    );
+  }, [id]);
+
   return (
     <div>
       <h2>{todo.title}</h2>
       <p>{todo.description}</p>
     </div>
   );
+}
+
+function Button({ id, onClick }) {
+  return <button onClick={onClick}>{id}</button>;
 }
 
 export default App;
