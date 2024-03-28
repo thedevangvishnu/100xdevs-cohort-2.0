daa344d47a43ae56696fc39c8b5065df03e69d51
Devang Vishnu <thedevangvishnu@gmail.com>
2024-03-28 12:03:17 +0530

    Create function to get user from "User" table

--- week-10/10.2-prisma/live-class-code/prisma-node-app/src/index.ts
+++ week-10/10.2-prisma/live-class-code/prisma-node-app/src/index.ts
@@ -2,6 +2,8 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+// 1: Write a function that let’s you insert data in the Users  table.
+
 async function insertUser(
   email: string,
   password: string,
@@ -27,6 +29,8 @@ async function insertUser(
 
 // insertUser("one@example.com", "one@1234", "oneFirst", "oneLast");
 // insertUser("two@example.com", "two@1234", "twoFirst", "twoLast");
+
+// 2: Write a function that let’s you update data in the Users table.
 
 interface UpdateParams {
   firstName: string;
@@ -54,7 +58,24 @@ async function updateUser(
   console.log(res);
 }
 
-updateUser("one@example.com", {
-  firstName: "oneFirstName",
-  lastName: "oneLastName",
-});
+// updateUser("one@example.com", {
+//   firstName: "oneFirstName",
+//   lastName: "oneLastName",
+// });
+
+// 3: Write a function that let’s you fetch the details of a user given their email
+
+async function getUser(username: string) {
+  const res = await prisma.user.findFirst({
+    where: { email: username },
+    select: {
+      id: true,
+      email: true,
+      firstName: true,
+    },
+  });
+
+  console.log(res);
+}
+
+getUser("two@example.com");
