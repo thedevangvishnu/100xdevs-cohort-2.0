91293019fb1fa204b6bcd074a2af52935393a2b0
Devang Vishnu <thedevangvishnu@gmail.com>
2024-04-12 09:51:38 +0530

    Bootstrap a nextjs with shadcn ui to buld a nextauth project

---
+++ week-16/16.4-nextauth/full-auth-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="destructive" size="lg">
+        Click me
+      </Button>
+    </div>
+  );
+}
