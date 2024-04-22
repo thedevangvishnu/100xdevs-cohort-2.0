de244fa1ba41f1028b2364cccab4ac621c022daf
Devang Vishnu <thedevangvishnu@gmail.com>
2024-04-22 08:43:13 +0530

    Update user model and add merchants model

--- week-17/paytm-project/packages/db/src/index.ts
+++ week-17/paytm-project/packages/db/src/index.ts
@@ -1,1 +1,11 @@
+import { PrismaClient } from "@prisma/client/extension";
+
 export * from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "productioin") globalThis.prisma = db;
